abd868bb1f9645424bcd7afe141d416350a2932a
Jacob Kindall <jacobfoxthecomposer@gmail.com>
2026-05-01 15:56:44 -0400

    Auto-deploy: 2026-05-01 15:56:44

--- src/features/citation-insights/SourceCategoryBreakdown.tsx
+++ src/features/citation-insights/SourceCategoryBreakdown.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import { motion } from "framer-motion";
 import { Card } from "@/components/atoms/Card";
 import { HoverHint } from "@/components/atoms/HoverHint";
+import { ChartExplainer } from "@/components/atoms/ChartExplainer";
 import { Layers, Info } from "lucide-react";
 import {
   CATEGORY_COLOR,
